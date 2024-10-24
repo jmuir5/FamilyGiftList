2a1ce9e430166f6b520caf29b70b631da0ef13eb
James Muir <noxappdev@gmail.com>
2024-10-24 22:37:39 +1100

    finished log in data pull

--- app/src/main/java/com/noxapps/familygiftlist/mygifts/SingleGiftPage.kt
+++ app/src/main/java/com/noxapps/familygiftlist/mygifts/SingleGiftPage.kt
@@ -279,7 +279,7 @@ fun DeleteDialogue(dialogueState:MutableState<Boolean>, onConfirm: ()-> Unit){
     Dialog(
         onDismissRequest = {dialogueState.value = !dialogueState.value},
     ){
-        Card {
+        Box(){
 
         }
     }
